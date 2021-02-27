c7c5174b0838e6fa40a5a09624a737eeacc020f6
Chris Piker <cpiker@users.noreply.github.com>
2021-02-26 22:32:00 -0600

    Starting dastime wrapper

--- das2/package.d
+++ das2/package.d
@@ -1,3 +1,9 @@
 // das2 D interface
 
 static if 
+
+
+das_init(toStringz(args[0]), DASERR_DIS_EXIT, 0, DASLOG_INFO, null);
+
+import das2.units;
+import das2.time;
--- test/test_units.d
+++ test/test_units.d
@@ -28,49 +28,46 @@
 
 import std.stdio;
 import std.string;
-import das2.units;
+import das2;
 
 int main(string[] args) {
-
-	/* Exit on errors, log info messages and above */
-	das_init(toStringz(args[0]), DASERR_DIS_EXIT, 0, DASLOG_INFO, null);
 	
 	/* Test singleton nature of unit values */
-	units Hz1 = Units("Hz");
+	Units Hz1 = Units("Hz");
 	string sHz2 = "Hz";
-	units Hz2 = Units(sHz2);
+	Units Hz2 = Units(sHz2);
 	if( (Hz1 != Hz2) || (Hz2 != UNIT_HERTZ)){
 		writef("ERROR: Test 1 Failed, %s != %s\n", Hz1, Hz2);
 		return 15;
 	}
 	
 	/* Test US2000 forward transformations */
-	Units units = UNIT_US2000;
-	Time dt = "2000-1-1T1:00";
+	Units us2000 = UNIT_US2000;
+	DasTime dt = DasTime("2000-1-1T1:00");
 	if(dt.valid)){
 		writef("ERROR: Test 2 Failed, can't parse %s as a string\n", sTime);
 		return 15;
 	}	
-	double rTime = units.fromTime(&dt);
+	double rTime = us2000.convert(&dt);
 	if( rTime != 3600000000.0){
 		writef("ERROR: Test 2 Failed, %s != %f μs since 2000-01-01\n",
 				sTime, rTime);
 		return 15;
 	}
-	double ssm = secondsSinceMidnight(rTime, units);
+	double ssm = us2000.secondsSinceMidnight(rTime);
 	if(ssm != 3600.0 ){
 		writef("ERROR: Test 2 Failed, %f US2000 is not %f seconds since midnight\n", 
 				 rTime, ssm);
 		return 15;
 	}
-	int jd = getJulianDay(rTime, units);
+	int jd = us2000.getJulianDay(rTime);
 	if(jd != 2451545){
 		writef("ERROR: Test 2 Failed, %s is not jullian day %d\n", sTime, jd);
 		return 15;
 	}
 
 	/* Test US2000 backward transformations */
-	dt = units.toTime(rTime);
+	dt = us2000.toTime(rTime);
 	string sBuf = dt.isoc();
 	if(sBuf != "2000-01-01T01:00:00"){
 		writef("ERROR: Test 3 Failed, %s != %f US2000\n", sBuf, rTime);
@@ -91,7 +88,7 @@ int main(string[] args) {
 		return 15;
 	}
 
-	dt = "1958-01-01T13:00";
+	dt = DasTime("1958-01-01T13:00");
 	if(!dt.valid)){
 		writef("ERROR: Test 5 Failed, can't parse %s as a string\n", sTime);
 		return 15;
@@ -102,16 +99,16 @@ int main(string[] args) {
 		return 15;
 	}
 	
-	/* Test conversion form MJ1958 to US2000 */
-	double rUs2000 = mj1958.convert(UNIT_US2000, rTime, units);
+	/* Test conversion from MJ1958 to US2000 */
+	double rUs2000 = UNIT_US2000.convert(rTime, UNIT_MJ1958);
 	if( rUs2000 != -1325329200000000.0){ 
 		writef("ERROR: Test 6 Failed, %f MJ1958 != %f US2000\n", rTime, rUs2000);
 		return 15;
 	}
 	
 	/* Test SSM and Julian Day */
-	double rSsm = Units_secondsSinceMidnight(rTime, units);
-	int nJd  = Units_getJulianDay(rTime, units);
+	double rSsm = mj1958.secondsSinceMidnight(rTime);
+	int nJd  = mj1958.getJulianDay(rTime);
 
 	if( rSsm != 46800){
 		writef("ERROR: Test 7 Failed, %f MJ1958 is not %f seconds since midnight\n",
@@ -126,9 +123,8 @@ int main(string[] args) {
 	
 	/* Test MJ1958 backward transformation */
 	rTime = 0.541667;
-	das_time dt1 = {0};
-	Units_convertToDt(&dt1, rTime, units);
-	dt_isod(sBuf.ptr, sBuf.length, &dt1, 0);
+	DasTime dt1 = mj1958.toTime(rTime);
+	sBuf = dt1.isod(0);
 	if(sBuf != "1958-001T13:00:00"){
 		writef("ERROR: Test 8 Failed, %f MJ1958 is not %s UTC\n", rTime, sBuf);
 		return 15;
@@ -136,58 +132,59 @@ int main(string[] args) {
 	
 	/* Test basic string parsing into canonical representation without
 	   unit reduction */
-	das_units a = Units_fromStr("V/m");
-	das_units b = Units_fromStr("V m^-1");
-	das_units c = Units_fromStr("V m**-2/2"); /*<-- don't use this, but it does work */
+	Units a = Units("V/m");
+	Units b = Units("V m^-1");
+	Units c = Units("V m**-2/2"); /*<-- don't use this, but it does work */
 	
 	if( a != b ){ writef("ERROR: Test 8 Failed, '%s' != '%s' \n", a, b); return 15; }
 	if( a != c ){ writef("ERROR: Test 8 Failed, '%s' != '%s' \n", a, c); return 15; }
 	
 	
 	/* Test unit inversion */
-	das_units d = Units_fromStr("m V**-1");
-	das_units e = Units_invert(a);
+	Units d = Units("m V**-1");
+	Units e = a^^-1;
 	
 	if( d != e ){ writef("ERROR: Test 9 Failed, '%s' != '%s' \n", d, e); return 15; }
 	
 	/* Test unit raise to power */
-	das_units f = Units_fromStr("V**2 m**-2");
-	das_units g = Units_power(a, 2);	
+	Units f = Units("V**2 m**-2");
+	Units g = a^^2;	
 	
 	if( f != g ){ writef("ERROR: Test 10 Failed, '%s' != '%s' \n", f, g); return 15; }
 	
 	/* Test unit multiplication */
-	das_units h = UNIT_E_SPECDENS;
+	Units h = UNIT_E_SPECDENS;
 	
-	das_units i = Units_multiply( Units_power(a, 2), Units_power(UNIT_HERTZ, -1));
+	Units i = Units_multiply( Units_power(a, 2), Units_power(UNIT_HERTZ, -1));
+	Units i = a^^2 * UNIT_HERTZ^^-1;
 	
 	if( h != i ){ writef("ERROR: Test 11 Failed, '%s' != '%s' \n", h, i); return 15; }
 	
 	/* Test interval units for t2000 */
-	das_units j = Units_interval(UNIT_T2000);
-	das_units k = Units_invert( Units_fromStr("Hertz") );
+	Units j = UNIT_T2000.interval();
+	Units k = Units("Hertz").invert();
 	if( j != k ){ writef("ERROR: Test 12 Failed, '%s' != '%s' \n", j, k); return 15; }
 	
 	/* Test interval units for us2000 */
-	das_units l = Units_interval(UNIT_US2000);
-	das_units m = Units_invert( Units_fromStr("MHz") );
+	Units l = UNIT_US2000.interval();
+	Units m = Units.invert( Units("MHz") );
 	if( l != m ){ writef("ERROR: Test 13 Failed, '%s' != '%s' \n", l, m); return 15; }
 	
 	
 	/* Test unit conversions */
-	das_units ms = Units_fromStr("microsecond");
-	das_units delta = Units_invert( ms );
-	double rFactor = Units_convertTo(UNIT_HERTZ, 1.0, delta);
+	Units ms = Units("microsecond");
+	Units delta = ms**-1;
+	double rFactor = UNIT_HERTZ.convert(1.0, delta);
 	if( rFactor != 1.0e+6){ 
 		write("ERROR: Test 14 Failed, '%s' to '%s' factor = %.1e, expected 1.0e+06\n", 
 				 delta, UNIT_HERTZ, rFactor);
 		return 15;
 	}
 	
-	das_units perday = Units_fromStr("kilodonut/day");
-	das_units persec = Units_fromStr("donut hertz");
+	Units perday = Units("kilodonut/day");
+	Units persec = Units("donut hertz");
 	
-	double rTo = Units_convertTo(persec, 86.4, perday);
+	double rTo = persec.convert(86.4, perday);
 	if( rTo != 1.0){
 		writef("ERROR: Test 15 Failed, 86.4 %s is %.4f %s, expected 1.0\n", perday,
 				 rTo, persec);
@@ -199,11 +196,11 @@ int main(string[] args) {
 	 * to work properly.  Reduction collapses all units to basic types with no
 	 * SI prefixes and then returns a factor that can be used to adjust values
 	 * in the non-reduced units to the reduced representation */
-	das_units O = Units_fromStr("ohms");
-	das_units O_reduced = Units_reduce(O, &rFactor);
-	das_units muO = Units_fromStr("μΩ");
+	Units O = Units("ohms");
+	Units O_reduced = O.reduce(rFactor);
+	Units muO = Units("μΩ");
 	
-	das_units muO_reduced = Units_reduce(muO, &rFactor);
+	Units muO_reduced = mu0.reduce(rFactor);
 	if(O_reduced != muO_reduced ){
 		writef("ERROR: Test 16 Failed, %s != %s\n", O_reduced, muO_reduced);
 		return 15;
@@ -215,10 +212,10 @@ int main(string[] args) {
 	}
 	
 	/* Test unicode decomposition for the special characters μ and Ω */
-	das_units bad_microohms  = Units_fromStr("µΩ m^-1"); /* Depending on the font in your  */
-	das_units good_microohms = Units_fromStr("μΩ m^-1"); /* editor you might not the       */
-	                                                    /* difference, but it's there and */
-	                                                    /* libdas2 handles it.            */
+	Units bad_microohms  = Units("µΩ m^-1"); /* Depending on the font in your   */
+	Units good_microohms = Units("μΩ m^-1"); /* editor you might not notice the */
+	                                         /* difference, but it's there and  */
+	                                         /* das2 handles it.                */
 	if( bad_microohms != good_microohms){
 		writef("ERROR: Test 18 Failed, decomposition failed %s != %s\n", bad_microohms,
 		       good_microohms);
@@ -228,9 +225,9 @@ int main(string[] args) {
 	/* Test order preservation for unknown units.  Assume that the user wants
 	 * units in the given order unless otherwise specified. */
 	string sUnits = "cm**-2 keV**-1 s**-1 sr**-1";
-	das_units flux = Units_fromStr(toStringz(sUnits));
+	Units flux = Units(sUnits);
 	
-	if(sUnits  flux) != 0){
+	if(sUnits != flux) != 0){
 		writef("ERROR: Test 19 Failed, unknown units are re-arranged by default. %s != %s\n",
 				 sUnits, flux);
 		return 15;
@@ -238,8 +235,8 @@ int main(string[] args) {
 	
 	/* New unit strings are sticky.  Test that a new variation of the new units
 	 * defined above reuses the first definition */
-	const char* sSameUnits = "hertz / kiloelectronvolt / centimeters^2 / sterradian";
-	das_units flux2 = Units_fromStr(sSameUnits);
+	string sSameUnits = "hertz / kiloelectronvolt / centimeters^2 / sterradian";
+	Units flux2 = Units(sSameUnits);
 	if( flux2 != flux){
 		writef("ERROR: Test 20 Failed, repeated unknown units are not normalized to "~
 				"first instance, %s != %s\n", flux2, flux);
@@ -250,9 +247,9 @@ int main(string[] args) {
 	
 	/* from Aspera reader... */
 	sUnits = "eV/(cm**-2 s**1 sr**1 eV**1)";
-	das_units energy_flux = Units_fromStr(sUnits);
-	das_units test_e_flux = Units_fromStr("m**2 s**-1 sr**-1");
-	das_units reduced_flux = Units_reduce(energy_flux, &rFactor);
+	Units energy_flux = Units(sUnits);
+	Units test_e_flux = Units("m**2 s**-1 sr**-1");
+	Units reduced_flux = energy_flux.reduce(&rFactor);
 	
 	if( reduced_flux != test_e_flux){
 		writef("ERROR: Test 21 Failed, eV did not cancel: %s (expected %s)\n",
@@ -262,8 +259,8 @@ int main(string[] args) {
 	
 	/* from Cassini density reader ... */
 	sUnits = "electrons / cm ^ 3";
-	das_units num_dens1 = Units_fromStr(sUnits);
-	das_units num_dens2 = Units_fromStr("electrons cm**-3");
+	Units num_dens1 = Units(sUnits);
+	Units num_dens2 = Units("electrons cm**-3");
 	if( num_dens1 != num_dens2){
 		writef("ERROR: Test 22 Failed, %s != %s", num_dens1, num_dens2);
 		return 15;
