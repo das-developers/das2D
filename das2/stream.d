839bd5e66294b607cb62f6939ebf71095174d51b
Chris Piker <cpiker@users.noreply.github.com>
2021-09-09 21:00:03 -0500

    Properties parsing with alternate language support

--- das2/stream.d
+++ das2/stream.d
@@ -6,6 +6,7 @@ import std.mmfile: MmFile;
 import std.file: exists, isFile;
 import std.experimental.logger;
 import dxml.parser; //parseXML, simpleXML, EntityType, Entity;
+import dxml.dom;    //
 import std.range;
 import std.algorithm: filter;
 import std.format : format;
@@ -19,16 +20,17 @@ import das2.units;
 import das2c.tt2000: das_tt2K_to_utc;
 
 alias XmlStream = EntityRange!(simpleXML, const(char)[]);
+alias DomObj = DOMEntity!(XmlStream.Input);
 
 /* ************************************************************************* */
-class DasTypeException : Exception
+class TypeException : Exception
 {
 package: // Only stuff in the das2 package can throw these
 	this(	string msg, string file = __FILE__, size_t line = __LINE__) @safe pure {
 		super(format("[%s,%s] %s", file, line, msg));
 	}
 }
-class DasStreamException : Exception
+class StreamException : Exception
 {
 package: // Only stuff in the das2 package can throw these
 this(	string msg, string file = __FILE__, size_t line = __LINE__) @safe pure {
@@ -38,7 +40,9 @@ this(	string msg, string file = __FILE__, size_t line = __LINE__) @safe pure {
 
 /* ************************************************************************* */
 
-enum PropType {TIME, STR, DATUM, DATUM_RNG, ORD, REAL};
+enum DimAxis {X, Y, Z, W};
+
+enum PropType {STR, DATUM, BOOL, INT, REAL, DATUM_RNG, REAL_RNG};
 
 /++ Items have a intrinsic encoding, but also a semantic meaning.  For example
  + the string "2019-001T14:00" is a 14-byte array, but it is also a UTC time
@@ -47,6 +51,89 @@ enum PropType {TIME, STR, DATUM, DATUM_RNG, ORD, REAL};
 struct PktProp{
 	PropType type = PropType.STR;
 	string value;
+	string[string] alt;  //keys are language codes
+}
+
+size_t setProperties(PktProp[string] props, DomObj root)
+{
+	size_t uPropsSet = 0;
+
+	foreach(elProp; root.children){
+		if(elProp.type != EntityType.elementStart) continue;
+		if(elProp.name != "p") continue;  // Ignore what you don't understand
+
+		string name;
+		PropType type = PropType.STR;
+		foreach(attr; elProp.attributes){
+			if(attr.name == "name"){ name = attr.name.dup; continue;}
+			if(attr.name == "type"){
+				switch(attr.value){
+				case "Datum": type = PropType.DATUM; break;
+				case "boolean": type = PropType.BOOL; break;
+				case "int": type = PropType.INT; break;
+				case "double": type = PropType.REAL; break;
+				case "DatumRange": type = PropType.DATUM_RNG; break;
+				case "doubleRange": type = PropType.REAL_RNG; break;
+				default:
+					throw new StreamException(format(
+						"Unknown property type '%s' at line %d", attr.value, elProp.pos.line
+					));
+				}
+			}
+		}
+
+		if(name.length == 0)
+			throw new StreamException(format(
+				"Attribute name missing from property at line %d", elProp.pos.line
+			));
+
+		// For string property parsing, we have the initial text, then alternate
+		// languages.
+
+		props[name] = PktProp.init;
+		props[name].type = type;
+		++uPropsSet;
+		
+		// See if any alternate language versions were set, if so save them
+		foreach(subItem; elProp.children){
+			if(subItem.type == EntityType.text)
+				props[name].value = subItem.text.dup;
+
+			if(subItem.type == EntityType.elementStart){
+				if(subItem.name != "alt") continue;
+
+				if(type != PropType.STR)
+					throw new StreamException(format(
+						"Property '%s' at line %d contains an alternate "~
+						"langage setting, but is not a string property ",
+						name, subItem.pos.line
+					));
+
+				auto attr = find!(a=> (a.name == "lang"))(subItem.attributes);
+				if(attr.empty)
+					throw new StreamException(format(
+						"'lang' attribute missing from <alt> element of "~
+						"property '%s' at line %d", name, subItem.pos.line
+					));
+				string lang = attr.front.value.dup;
+
+				foreach(altchild; subItem.children){
+					if(altchild.type == EntityType.text)
+						props[name].alt[lang] = altchild.text.dup;
+				}
+			}
+		}
+
+	}
+
+	return uPropsSet;
+}
+
+size_t setProperties(PktProp[string] props, XmlStream rXml)
+{
+	auto dom = parseDOM(rXml);
+	auto root = dom.children[0];
+	return setProperties(props, root);
 }
 
 /* ************************************************************************* */
@@ -67,8 +154,29 @@ private:
 
 	char cFieldSep;
 	Units _units;
+
+	ubyte[] _delim;
+	int _items;
 	
 public:
+
+	this(DomObj root, const(ubyte)[] delim)
+	{
+		if(delim.length > 0) _delim = delim.dup;
+
+		throw new StreamException("Array construction");
+	}
+
+	const(ubyte)[] setData(const(ubyte)[] _data)
+	{
+		// Cast the byte array to the appropriate buffer type, read data byte
+		// swapping if necessary.  Keep track of the number of bytes read and 
+		// return a shortened slice.
+
+
+		return _data;
+	}
+
 	long vint(){
 		switch(_buf_type){
 		case EncodingType.CHARS:   return to!long( (cast(const(char)[]) _rawdata)[0] );
@@ -95,7 +203,7 @@ public:
 	DasTime vtime(){
 		if(_units == UNIT_UTC){
 			if(_buf_type != EncodingType.CHARS)
-				throw new DasTypeException("units=\"UTC\" but raw data are not characters");
+				throw new TypeException("units=\"UTC\" but raw data are not characters");
 			return DasTime(cast(const(char)[]) _rawdata);
 		}
 
@@ -124,7 +232,7 @@ public:
 			return _units.toTime(d);
 		}
 
-		throw new DasTypeException(format(
+		throw new TypeException(format(
 			"Values in units=\"%s\" are not datetimes", _units ));
 	}
 
@@ -161,7 +269,7 @@ public:
 
 		case SemanticType.TIMES:
 			if(_units == UNIT_UTC)
-				throw new DasTypeException(format(
+				throw new TypeException(format(
 					"ISO time strings not comparible to %s", typeid(T)
 				));
 					
@@ -178,7 +286,7 @@ public:
 				return 0;				
 			}
 		default:	
-			throw new DasTypeException(format(
+			throw new TypeException(format(
 				"Text strings are not comparible to %s", typeid(T))
 			);
 		}
@@ -186,7 +294,7 @@ public:
 
 	int opCmp(T)(auto ref const T other) if( is(T:DasTime) ){
 		if(_semantic != SemanticType.TIMES)
-			throw new DasTypeException("None time values can't be compared to a DasTime");
+			throw new TypeException("None time values can't be compared to a DasTime");
 		DasTime dt = vtime();
 		return dt.opCmp(other);
 	}
@@ -195,33 +303,105 @@ public:
 /* ************************************************************************* */
 
 struct PktDim {
-	PktProp[string] props;
-	PktAry[string] arrays;
+	PktProp[string] _props;
+	PktAry[string] _arrays;  //one array for each usage
+
+	string[] _readOrder;
+
+	this(DomObj root, PktProp[string] streamProps, ubyte[] delim){
+		// Merge the stream props in with my props, then override later
+		foreach(key, val; streamProps)
+			_props[key] = val;
+		
+		foreach(el; root.children){
+			if(el.name == "properties")
+				setProperties(_props, el);
+
+			if(el.name == "array"){
+				string usage = "center";
+				auto attr = find!(a=> (a.name == "usage"))(el.attributes);
+				if(!attr.empty) usage = attr.front.value.dup;
+				
+				_arrays[usage] = PktAry(el, delim);
+				_readOrder[$] = usage;
+			}
+
+			if(el.name == "xcoord" || el.name == "ycoord" ||el.name == "zcoord"){
+				throw new StreamException(format(
+					"Offset coordinate handling is yet implemented for '%s' at line %d",
+					el.name, el.pos.line
+				));
+			}
+
+			// Ignore anything else
+		}
+	}
+
+	// Initialize PktDim objects using sections of the data
+	const(ubyte)[] setData(const(ubyte)[] _data)
+	{
+		for(int i = 0; i < _readOrder.length; ++i){
+			_data = _arrays[_readOrder[i]].setData(_data);
+		}
+
+		return _data;
+	}
+
 	ref PktAry opIndex(string sUsage){
-		return arrays[sUsage];
+		return _arrays[sUsage];
 	}
 }
 
 /* ************************************************************************* */
 
 struct DasPkt {
-	PktDim[string] dims;
+	PktDim[string] _dims;
+	string[] _readOrder;
+	ubyte[] _delim;
 
-package:
-	this(XmlStream)
+	this(XmlStream rXml, PktProp[string] streamProps)
 	{
+		auto dom = parseDOM(rXml);
+		auto root = dom.children[0];
+		foreach(attr; root.attributes){
+			if( attr.name == "delim" && attr.value == "space"){
+				_delim[0] = ' ';
+				break;
+			}
+		}
 
+		foreach(pdim; root.children){
+			if(pdim.name == "yset" || pdim.name == "zset" || pdim.name == "wset")
+				throw new StreamException("yset, zset & wset reading not yet implemented");
+
+			
+			if(pdim.name != "x" && pdim.name != "y" && pdim.name != "z")
+				throw new StreamException(format(
+					"Skipping custom content, '%s' is not yet implemented", pdim.name
+				));
+
+			auto attr = find!(a=> (a.name == "pdim"))(pdim.attributes);
+			if(attr.empty)
+				throw new StreamException(format("Required attribute 'pdim' missing from element '%s'", pdim.name));
+			
+			_readOrder[$] = attr.front.name.dup;
+			_dims[_readOrder[$-1]] = PktDim(pdim, streamProps, _delim);
+		}
 	}
 
 	// Initialize PktDim objects using sections of the data
 	void setData(const(ubyte)[] _data)
 	{
-
+		for(int i = 0; i < _readOrder.length; ++i){
+			// each dimension must eat all deliminators appearing before
+			// and after it in the stream.
+			_data = _dims[_readOrder[i]].setData(_data);
+		}
 	}
 
 	ref PktAry opIndex(string sDim, string sUsage="center")
 	{
-		return dims[sDim][sUsage];
+		return _dims[sDim][sUsage];
 	}
 }
 
@@ -250,7 +430,7 @@ package:
 		if(_rXml.front.name == "h" || _rXml.front.name == "d"){
 			auto attr = find!(a=> (a.name == "id"))(_rXml.front.attributes);
 			if(attr.empty){
-				throw new DasStreamException(format(
+				throw new StreamException(format(
 					"[%s:%d] The 'id' attribute is missing from the XML header"~
 					"packet container", _source, _rXml.front.pos.line
 				));
@@ -259,7 +439,7 @@ package:
 		}
 		
 		// Wierd stuff 
-		throw new DasStreamException(format(
+		throw new StreamException(format(
 			"[%s:%d] Unknown packet tag element '%s'",
 			_source, _rXml.front.pos.line, _rXml.front.name
 		));
@@ -286,11 +466,6 @@ public:
 
 		// Iterate to next data packet
 		popFront();
-	}
-
-	void setProps(XmlStream rXml)
-	{
-
 	}
 
 	@property bool empty() { return (_curPktId > 0); }
@@ -321,13 +496,13 @@ public:
 				case "stream":
 					// Subsequent stream headers are okay, so long as they don't
 					// change the format
-					setProps(_rXml);
+					setProperties(_props, _rXml);
 					continue NEXTPKT;
 				case "packet":
-					_pkts[tag.id] = DasPkt(_rXml);
+					_pkts[tag.id] = DasPkt(_rXml, _props);
 					break NEXTPKT;
 				default:
-					throw new DasStreamException(format(
+					throw new StreamException(format(
 						"[%s:%d] Unexpected header element '%s'", _source, 
 						_rXml.front.pos.line, _rXml.front.name
 					));
@@ -337,7 +512,7 @@ public:
 			// Data
 			if(tag.type == 'd'){
 				if(tag.id !in _pkts)
-					throw new DasStreamException(format(
+					throw new StreamException(format(
 						"[%s:%d] Data packet id='%d' received before "~
 						"header packet %s", _source, _rXml.front.pos.line, tag.id
 					));
