5a1a92f0b2d78bdc0eede5df9c47381a780c75de
Chris Piker <cpiker@users.noreply.github.com>
2023-01-06 15:23:51 -0600

    Draft BitReader interface

--- das2/producer.d
+++ das2/producer.d
@@ -15,10 +15,11 @@ module das2.producer;
 
 import core.stdc.stdlib: exit;
 
-import std.algorithm: copy, map;
+import std.algorithm: canFind, copy, map;
 import std.array:     appender, join;
 import std.bitmanip:  write;
 import std.conv:      ConvException, to;
+import std.exception: enforce;
 import std.format:    format, sformat;
 import std.getopt:    getopt, config, GetoptResult, Option;
 import std.range:     ElementType;
@@ -35,10 +36,40 @@ import das2.log:      errorf;
 import das2.time:     DasTime;
 import das2.units:    Units, UNIT_DIMENSIONLESS;
 
-/+ Set the output format for exceptions, properties, etc. +/
+private alias r = representation;
+
+/* Basic stream definitions *********************************************** */
+
+alias BE = Endian.bigEndian;
+alias LE = Endian.littleEndian;
+
+/++ An enumeration of the stream versions understood by this module +/
 enum StreamFmt {INVALID=0, v22=220, v30=300 };
 
-private alias r = representation;
+/++ The legal property types for das3 streams as extracted from:
+ +   das-basic-stream-v3.0.xsd  
+ + which is the authoritative source of property types for das3 streams.
+ +/
+enum PropType {
+ 	STRING, BOOL, DATETIME, DATETIME_RNG, INT, INT_RNG, REAL, REAL_RNG
+};
+
+/++ Flags corresponding to the output types given in:
+ +     das-basic-stream-v3.0.xsd  
+ +  which is the authoritative upstream source.
+ +/
+enum EncodingType {
+	UBYTE, UTF8, BE_INT, BE_UINT, BE_REAL, LE_INT, LE_UINT, LE_REAL
+};
+
+enum TagType {INVALID=0, Sx = 1, Hx = 2, Pd = 3, Cx = 4, Ex = 5, XX = 6 };
+
+/++ Flags corresponding to the client semantic types given in:
+ +     das-basic-stream-v3.0.xsd  
+ +  which is the authoritative upstream source.
+ +/
+private enum ValueType {UNKNOWN, BOOL, DATETIME, INT, REAL, STRING };
+
 
 /* Reader command line assistance ***************************************** */
 
@@ -217,11 +248,6 @@ private S _breakNrap(S)(
 
 /* Packet Buffering and Tagging ******************************************* */
 
-alias BE = Endian.bigEndian;
-alias LE = Endian.littleEndian;
-
-enum TagType {INVALID=0, Sx = 1, Hx = 2, Pd = 3, Cx = 4, Ex = 5, XX = 6 };
-
 // Das stream exception types (move this somewhere else)
 enum StreamExc{ Query, Server, NoData };
 
@@ -250,7 +276,7 @@ string toString(StreamFmt SV)(StreamExc et){
  +
  + This is a stack memory optimized writer.  A single buffer is used
  + for each instance of this structure. +/
-struct PktBuf(size_t buf_sz = 65536, StreamFmt SV = StreamFmt.V30 )
+struct PktBuf(size_t buf_sz = 65536, StreamFmt SV = StreamFmt.v30 )
 {
 	ubyte[buf_sz] _buf;        
 	/* Leave room for a tag with 2 tag bytes, 4 pipe bytes, 10 len bytes
@@ -384,26 +410,11 @@ struct PktBuf(size_t buf_sz = 65536, StreamFmt SV = StreamFmt.V30 )
 	}
 }
 
-/* Common packet emits **************************************************** */
+/* Stream Properties ****************************************************** */
 
-/* Emitt a query error as a das packet
- *
- * Params: 
- *   opts = A list of options returned from getopt
- *   width = The total print width in columns, used for text wrapping
- *   indent = The number of columns to leave blank before each line
- *   subIndent = The number of columns to leave blank before the help
- *        text of an item.  This is in addition to the overall indention
- * Returns: The value 13, always.
- */
-
-enum PropType {
- 	STRING, BOOL, DATETIME, DATETIME_RNG, INT, INT_RNG, REAL, REAL_RNG
-};
-
-/+ A simple das properties class, holds a property with it's name 
-   type and units +/
-
+/++ A simple das properties class, holds a property with it's name type and 
+ + units
+ +/
 struct Property{
 	string name;
 	PropType type;
@@ -638,4 +649,189 @@ int writeException(StreamFmt SF)(StreamExc et, string sMsg)
 		
 	errorf("%s", sMsg.strip());
 	return 13;
+}
+
+/* Parsing & Streaming Binary Data **************************************** */
+
+/+ Handle extracting bit fields to a buffer.
+ +
+ + BitReaders are useful for translating bit-fields into das stream values.
+ + The various valuetypes and encoding types are taken from:
+ +
+ +   das-basic-stream-v3.0.xsd
+ +   das-basic-stream-v2.2.xsd
+ +
+ + in the das2docs repoistory, which are the authoritative source of encodings
+ + for das3 and das2 streams streams repectively
+ +
+ + Examples:
+ + --------------
+ + ubyte[] pDat;  // The upstream data buffer
+ + 
+ + BitReader seqNo = BitReader(
+ +   "SeqNo", 11, 3, "LEuint", 2, "bool", "Packet Sequence Number"
+ + );
+ + BitReader rawTemp = BitReader(
+ +   "TMON", 12, 200, "LEuint", 2, "int", "LF preamp 1 temperature"
+ + );
+ +
+ + // Write L0 (non calibrated data)
+ + PktBuf(800, StreamFmt.v30) packet;
+ + packet.write(pDat.seqNo, pDat.rawTemp);
+ +
+ + // Write L1 (calibrated data using polynomial set)
+ + CalReader celsius = CalReader(rawTemp, [26.4, 20.0], "°C");
+ + packet.write(pDat.celcius);
+ + --------------
+ +
+ +/
+struct BitReader(StreamFmt SF = StreamFmt.v30){
+	string name;
+   ubyte  inBits;
+	size_t inOffset;
+	EncodingType encType;
+	ushort  encLen;
+	ValueType valType;
+	string title;
+	bool   ignore = true;
+	private ubyte[8] _buf = 0;
+
+	/++ Create a bit parser, minimal verision 
+	 +
+	 + Params:
+	 +   name = A short name for this field, should follow rules for a
+	 +      legal variable name in most languages
+	 +   inBits = The number of input bits in the field
+	 +   inOffset = The offset *in bits* at which the field starts in a blob
+	 +   dasEncode = A Das Encoding for the output, one of:
+	 +      "ubyte","","","",""
+	 +/
+	this(
+		string name, ubyte inBits, size_t inOffset, string encodeType, 
+		ubyte encodeLen, string valueType = "", string title = ""
+	){
+		this.name = name;
+		this.inBits = inBits;
+		this.inOffset = inOffset;
+
+		// These are taken from das-basic-stream-v3.0.xsd
+		switch(encodeType){
+			case "ubyte":  encType = EncodingType.UBYTE;   break;
+			case "utf8" :  encType = EncodingType.UTF8;    break;
+			case "BEint":  encType = EncodingType.BE_INT;  break;
+			case "BEuint": encType = EncodingType.BE_UINT; break;
+			case "BEreal": encType = EncodingType.BE_REAL; break;
+			case "LEint":  encType = EncodingType.LE_INT;  break;
+			case "LEuint": encType = EncodingType.LE_UINT; break;
+			case "LEreal": encType = EncodingType.LE_REAL; break;
+			default:
+				enforce(false, format!"Unknown encoding %s"(encodeType));
+		}
+
+		encLen = encodeLen;
+		if(encType != EncodingType.UBYTE && encType != EncodingType.UTF8)
+			enforce([1u, 2u, 4u, 8u].canFind(encLen), 
+				format!"Invalid encoding length of %d for %s"(encLen, encodeType)
+			);
+		
+		// These are taken from das-basic-stream-v3.0.xsd
+		switch(valueType){
+			case "bool":     valType = ValueType.BOOL; break;
+			case "datetime": valType = ValueType.DATETIME; break;
+			case "int":      valType = ValueType.INT; break;
+			case "real":     valType = ValueType.REAL; break;
+			case "string":   valType = ValueType.STRING; break;
+			default:         valType = ValueType.UNKNOWN; break;
+		}
+
+		this.title = title;
+	}
+
+	/++ Since das3 stream values are never smaller then 1-byte, return the 
+	 + bit field as a byte array, with the proper endianness +/
+   ubyte[] opCall(ubyte[] pIn){
+
+   	// Example Input: offset=19  length=11
+   	//             1          2          3       
+   	//  01234567 89012345 67890123 45678901 23456789
+   	// +--------+--------+--------+--------+--------+
+   	// |        |        |   XXXXX|XXXXXX  |        |
+   	// +--------+--------+--------+--------+--------+
+   	//
+   	// Example Output (endian retained):
+   	//
+   	//  01234567 89012345
+   	// +--------+--------+
+   	// |00000XXX|XXXXXXXX|
+   	// +--------+--------+
+   	//
+   	// Example Output (endian swapped):
+   	//
+   	//  01234567 89012345
+   	// +--------+--------+
+   	// |XXXXXXXX|00000XXX|
+   	// +--------+--------+
+
+   	// Algorithm:
+   	// * Take last two bytes of source as a ushort,
+   	//   ushort last2 = to!ushort(pIn[iEnd - 1 .. iEnd + 1]);
+   	//
+   	// * Downshift by uShift, truncate if bytes left 
+   	//   last2 = last 2 >> uShift
+
+
+   	// Just 0 everything for now to make code compile.
+   	return _buf[0 .. encLen];
+
+   	/*
+   	size_t iBeg = inOffset / 8;  // truncate down
+   	size_t iEnd = (inOffset + etLen)/8; // truncate down
+   	ushort uShift = 0;
+   	ubyte  uMask0 = 0xFF;
+   	if( iEnd*8 < inOffset + enLen){ 
+   		iEnd += 1;
+   		uShift = iEnd*8 - (inOffset + enLen);
+
+   	}
+
+
+
+
+   	else{
+   		// Positions to remove from top of byte0
+   		ushort uRm = 8 - ((inOffset - iBeg*8) + uShift);
+
+   	}
+
+   	pOut[0] = (pIn[iEnd] >> uShift) & uMask0;
+
+
+   	ubyte[] pRead = pIn[iBeg .. iEnd + 1];
+
+   	// Shift & patch starting from highest address
+   	if(uShift > 0){
+   		for(int i = pRead.length - 1; i > -1; --i){
+	   		pRead[i] = ;
+   		}
+   	}
+   	*/
+   }
+
+   /++ Return the bit field as a given value +/
+   //T as(T)(ubyte[] pIn){
+//
+//
+   //}
+}
+
+unittest{
+	ubyte[] aSrc = [0x30, 0x00];
+
+	immutable(StreamFmt) SF = StreamFmt.v30;
+
+	// Just test construction and function call (without error checking)
+	BitReader!SF flag = BitReader!SF("flag", 1, 3, "ubyte", 1, "bool" );
+
+	auto aRet = flag(aSrc[]);
+
 }
